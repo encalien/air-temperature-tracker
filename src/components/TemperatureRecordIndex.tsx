e57c1f0585cc49dcedcce1c84512c15745ebab4d
Katarina <katarina.mursec@gmail.com>
2023-05-03 10:53:49 +0200

    add basic pagination

--- src/components/TemperatureRecordIndex.tsx
+++ src/components/TemperatureRecordIndex.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import TemperatureRecord from "../types/TemperatureRecord";
 
 export default function TemperatureRecordIndex(
   { temperatureRecords }: { temperatureRecords: TemperatureRecord[] }
 ) {
+  const [visibleTemperatureRecords, setVisibleTemperatureRecords] = useState<TemperatureRecord[]>([]);
+  const [showAll, setShowAll] = useState<boolean>(false);
+  const [pageCount, setPageCount] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const recordsPerPage = 10;
+
+  const toggleShowAll = (): void => {
+    setShowAll(!showAll)
+  }
+
+  const setPageTemperatureRecords = (page: number): void => {
+    setCurrentPage(page);
+    const visibleRecords = showAll ? temperatureRecords
+      : temperatureRecords.slice(page * recordsPerPage, (page + 1) * recordsPerPage);
+    setVisibleTemperatureRecords(visibleRecords);
+  }
+
+  useEffect(() => {
+    setPageCount(Math.ceil(temperatureRecords.length / 10));
+    setPageTemperatureRecords(currentPage);
+  }, [temperatureRecords, showAll]);
+
   return (
     <div>
       <table>
@@ -14,7 +37,7 @@ export default function TemperatureRecordIndex(
           </tr>
         </thead>
         <tbody>
-          {temperatureRecords.map((record: TemperatureRecord, i: number) =>(
+          {visibleTemperatureRecords.map((record: TemperatureRecord, i: number) =>(
             <tr key={i}>
               <td>{new Date(record.time).toLocaleDateString('sl-SL')}</td>
               <td>{record.location}</td>
@@ -23,6 +46,12 @@ export default function TemperatureRecordIndex(
           ))}
         </tbody>
       </table>
+      <button onClick={toggleShowAll}>{showAll ? 'Show less' : 'Show all'}</button>
+      <div>
+        {Array.from(Array(pageCount).keys()).map((page: number) => (
+          <button key={page} onClick={() => setPageTemperatureRecords(page)}>{page + 1}</button>
+        ))}
+      </div>
     </div>
   );
 }
